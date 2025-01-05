94757a484423162b8b43ce9c8f0097d9dfbd9de8
Shangjie Zheng <shangjiez@mail.smu.edu>
2025-01-05 13:31:09 -0700

    Clean up uncommitted changes.

---
+++ src/main/java/org/supershop/shopcore/db/mappers/OrderMapper.java
@@ -0,0 +1,19 @@
+package org.supershop.shopcore.db.mappers;
+
+import org.supershop.shopcore.db.po.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+
+    Order selectByOrderNo(String orderNo);
+}
